88c9883b224006a9c44da55c20c55a5a67f94ee8
tsontang <tsontang@jidouauto.com>
2019-07-19 17:18:13 +0800

    添加bottomSheet，需要更自由的封装

--- app/src/main/java/com/tson/utils/photo/CameraActivity.kt
+++ app/src/main/java/com/tson/utils/photo/CameraActivity.kt
@@ -9,9 +9,12 @@ import com.tson.utils.R
 import com.tson.utils.lib.util.UtilsHelper
 import com.tson.utils.lib.util.camera.CameraCallback
 import com.tson.utils.lib.util.camera.CameraUtils
+import com.tson.utils.view.fragment.BottomSheet
 import kotlinx.android.synthetic.main.activity_camera.*
 
 class CameraActivity : AppCompatActivity() {
+
+    private var payForUser = BottomSheet<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +31,14 @@ class CameraActivity : AppCompatActivity() {
         open_camera.setOnClickListener {
             CameraUtils.openCamera(this@CameraActivity)
         }
+        bottomSheet.setOnClickListener {
+            val list = mutableListOf("1", "@", "1", "@", "1", "@", "1", "@", "1", "@", "1", "@", "1", "@", "1")
+            payForUser.setData(list, "收款对象", { _, result ->
+            }, { tv, position ->
+                tv.text = list[position]
+            })
+            payForUser.show(supportFragmentManager, CameraActivity::getLocalClassName.toString() + "1")
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -37,7 +48,7 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        CameraUtils.onActivityResult(requestCode, resultCode, data,  object : CameraCallback() {
+        CameraUtils.onActivityResult(requestCode, resultCode, data, object : CameraCallback() {
             override fun camera(bitmap: Bitmap?) {
             }
 
