50ac2eea9521a9906ed0c60c0bb1bb0f62803b65
tsontang <tsontang@jidouauto.com>
2019-08-30 15:06:42 +0800

    add test code

---
+++ app/src/main/java/com/tson/utils/Button4Activity.kt
@@ -0,0 +1,14 @@
+package com.tson.utils
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_button4.*
+
+class Button4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button4)
+        touch.setOnClickListener {  }
+    }
+}
